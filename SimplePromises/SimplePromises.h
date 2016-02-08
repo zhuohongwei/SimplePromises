95e3e8afc048314a72b614f6868f77c13d934c36
Zhuo Hong Wei <hongwei.zhuo@gmail.com>
2016-02-08 17:27:25 +0800

    Initial Commit

---
+++ SimplePromises/SimplePromises.h
@@ -0,0 +1,19 @@
+//
+//  SimplePromises.h
+//  SimplePromises
+//
+//  Created by Zhuo Hong Wei on 8/2/16.
+//  Copyright © 2016 Zhuo Hong Wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimplePromises.
+FOUNDATION_EXPORT double SimplePromisesVersionNumber;
+
+//! Project version string for SimplePromises.
+FOUNDATION_EXPORT const unsigned char SimplePromisesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimplePromises/PublicHeader.h>
+
+
---
+++ SimplePromisesTests/SimplePromisesTests.swift
@@ -0,0 +1,36 @@
+//
+//  SimplePromisesTests.swift
+//  SimplePromisesTests
+//
+//  Created by Zhuo Hong Wei on 8/2/16.
+//  Copyright © 2016 Zhuo Hong Wei. All rights reserved.
+//
+
+import XCTest
+@testable import SimplePromises
+
+class SimplePromisesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
